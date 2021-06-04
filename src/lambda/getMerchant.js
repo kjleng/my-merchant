9eec5fd3e1c8ef3a17c2adc98ba2ad24fb7304a0
Ed Leng <edmund.leng@slalom.com>
2021-06-03 21:09:17 -0700

    rrr

---
+++ src/lambda/getMerchant.js
@@ -0,0 +1,57 @@
+const { DynamoDB } = require('aws-sdk');
+
+const buildResponse = (result) => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: `${JSON.stringify(result, undefined, 2)}`
+  };
+};
+
+const buildNotFoundResponse = () => {
+  return {
+    statusCode: 404,
+    headers: { "Content-Type": "application/json" },
+    body: 'Not found'
+  };
+};
+
+const buildCannotProcessResponse = () => {
+  return {
+    statusCode: 422,
+    headers: { "Content-Type": "application/json" },
+    body: 'name is required'
+  };
+};
+
+const MERCHANT_MAPPING_TABLE = process.env.merchant_mapping_table;
+const REGION = process.env.region;
+
+const getNameFromParam = (event) => {
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    return event.queryStringParameters.name;
+  }
+};
+
+const handler = async (event) => {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  
+  const merchantName = getNameFromParam(event)
+  if (!merchantName) return buildCannotProcessResponse();  
+  const result = await getMerchantByName(merchantName);
+  console.log(`!!! Merchant retrieved: ${JSON.stringify(result)}`);
+
+  return result ? buildResponse(result) : buildNotFoundResponse();
+};
+
+const getMerchantByName = async (merchantName) => {
+  const docClient = new DynamoDB.DocumentClient({apiVersion: '2012-08-10', region: REGION});
+  return await docClient.query({
+    TableName: MERCHANT_MAPPING_TABLE,
+    IndexName: 'merchantName-index',
+    KeyConditionExpression: 'merchantName = :merchantName',
+    ExpressionAttributeValues: { ':merchantName': merchantName }
+  }).promise(); 
+};
+
+module.exports = { handler };
---
+++ stacks/main.js
@@ -0,0 +1,44 @@
+const { Stack } = require('@aws-cdk/core');
+const { Runtime, Code, Function } = require('@aws-cdk/aws-lambda');
+const { RestApi, LambdaIntegration } = require('@aws-cdk/aws-apigateway');
+const { Table, AttributeType } = require('@aws-cdk/aws-dynamodb');
+
+const runtime = Runtime.NODEJS_14_X;
+const LAMBDA_DIR = 'src/lambda';
+
+const getName = (resourceName, environment) => {
+  const _env = environment || 'qa';
+  return `${_env}-${resourceName}`;
+}
+
+class MainStack extends Stack {
+  constructor(scope, id, props, config) {
+    super(scope, id, props);
+
+    const { aws_account_id, region, environment, merchant_mapping_table } = config;
+    /** API Gateway **/
+    const merchantApi = new RestApi(this, getName('merchant-onboarding-api', environment), { deployOptions: { stageName: environment } });
+    const apiRoot = merchantApi.root.addResource('api');
+    const v1 = apiRoot.addResource('v1');
+    const merchants = v1.addResource('merchants');
+    
+    /*** Lambdas **/
+    const getMerchant = new Function(this, getName('get-merchant-function', environment), { 
+      runtime, 
+      code: Code.fromAsset(LAMBDA_DIR), 
+      handler: 'getMerchant.handler',
+      environment: config
+    });
+    merchants.addMethod('GET', new LambdaIntegration(getMerchant));
+    
+    /** DynamoDB **/
+    const merchantTableArn = `arn:aws:dynamodb:${region}:${aws_account_id}:table/${merchant_mapping_table}`;
+    const merchantMappingTable = Table.fromTableArn(this, getName('merchant-mapping-table', environment), merchantTableArn);
+    merchantMappingTable.grantReadData(getMerchant);
+
+    const merchantIndex = Table.fromTableAttributes(this, 'dev-merchant-index', { tableArn: merchantTableArn, globalIndexes: ['merchantName-index']});
+    merchantIndex.grantReadData(getMerchant);
+  }
+}
+
+module.exports = { MainStack }
