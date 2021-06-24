ab8081ca7abb69297c8c2ede2f4976c19ba6adc9
Ed Leng <edmund.leng@slalom.com>
2021-06-24 10:02:52 -0700

    email

--- stacks/main.js
+++ stacks/main.js
@@ -27,8 +27,18 @@ class MainStack extends Stack {
     const v1 = apiRoot.addResource('v1');
     const merchants = v1.addResource('merchants');
     const basicMerchants = v1.addResource('basic_merchants');
+    const signup = merchants.addResource('signup');
+    const emailMerc = merchants.addResource('email_merchant');
 
     /*** Lambdas **/
+    const sendEmail = new Function(this, getName('send-email', environment), { 
+      runtime, 
+      code: Code.fromAsset(LAMBDA_DIR), 
+      handler: 'sendEmail.handler',
+      environment: config
+    });
+    emailMerc.addMethod('GET', new LambdaIntegration(sendEmail));
+
     const getMerchant = new Function(this, getName('get-merchant-function', environment), { 
       runtime, 
       code: Code.fromAsset(LAMBDA_DIR), 
@@ -53,6 +63,14 @@ class MainStack extends Stack {
     });
     basicMerchants.addMethod('GET', new LambdaIntegration(getAllMerchantsFromUserPool));
     
+    const signFn = new Function(this, getName('sign-fn', environment), { 
+      runtime, 
+      code: Code.fromAsset(LAMBDA_DIR), 
+      handler: 'autoApproveMerchantUser.handler',
+      environment: config
+    });
+    signup.addMethod('GET', new LambdaIntegration(signFn));
+   
     /** Cognito **/
     const userPool = UserPool.fromUserPoolId(this, 'UserPool', 'us-east-1_6JDtBmeG2');
     getAllMerchantsFromUserPool.addToRolePolicy(
@@ -63,7 +81,18 @@ class MainStack extends Stack {
           `arn:aws:cognito-idp:${userPool.stack.region}:${userPool.stack.account}:userpool/${userPool.userPoolId}`
         ],
       })
-    )
+    );
+    signFn.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          'cognito-idp:*'
+        ],
+        resources: [
+          `arn:aws:cognito-idp:${userPool.stack.region}:${userPool.stack.account}:userpool/${userPool.userPoolId}`
+        ],
+      })
+    );
 
     /** DynamoDB **/
     const merchantTableArn = `arn:aws:dynamodb:${region}:${aws_account_id}:table/${merchant_mapping_table}`;
